d0a25e0a44d6697bc693ef8c57e1355b3ba787d8
David Binetti <dbinetti@gmail.com>
2021-05-09 19:11:13 -0600

    Update management command

--- project/app/management/commands/delete_test_accounts.py
+++ project/app/management/commands/delete_test_accounts.py
@@ -1,11 +1,14 @@
-from app.models import User
 from django.core.management.base import BaseCommand
+from django.db.models import Q
+
+from app.models import User
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         users = User.objects.filter(
-            email__endswith='smilewestada.com',
+            Q(email__endswith='smilewestada.com') |
+            Q(email__endswith='tfbnw.net')
         )
         for user in users:
             user.delete()
