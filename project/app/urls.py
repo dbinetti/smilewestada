e965d91754dfc837b5f94c0697874f8949593b34
David Binetti <dbinetti@gmail.com>
2021-05-19 22:29:40 -0600

    obscure

--- project/app/urls.py
+++ project/app/urls.py
@@ -32,9 +32,9 @@ urlpatterns = [
 
     # Share
     # path('share', views.share, name='share'),
-    path('comments', views.comments, name='comments'),
-    path('comment-submission', views.comment_submission, name='comment_submission'),
-    path('submit', views.submit, name='submit'),
+    # path('comments', views.comments, name='comments'),
+    # path('comment-submission', views.comment_submission, name='comment_submission'),
+    # path('submit', views.submit, name='submit'),
 
     # Delete
     path('delete', views.delete, name='delete',),
